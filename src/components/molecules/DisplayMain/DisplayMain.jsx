b0a6ce4257ce77017cf8d6275b95d4b317fab410
jesus-imanol <jesusimanolcastillo@gmail.com>
2024-05-19 00:19:53 -0600

    cambios en header y main

--- src/components/molecules/DisplayMain/DisplayMain.jsx
+++ src/components/molecules/DisplayMain/DisplayMain.jsx
@@ -2,10 +2,14 @@ import style from "./DisplayMain.module.css"
 import TitleIntro from "../../atoms/Texts/TextIntro";
 import AvatarText from "../../atoms/ImageMain/AvatarText";
 import IconAvatar from "../../atoms/ImageMain/IconAvatar";
+import TextCardMain from "../../atoms/Texts/TextCardMain";
 function DisplayMain() {
     return (
         <>
-        <div id={style.video_container}>
+        <div id={style.box_main}>
+            <div id={style.container_card}>
+                <TextCardMain val={"Conocénos!"} title={"Brazil, Rio de Janeiro" } text={"Disfruta nuestra cultura"}></TextCardMain>
+            </div>
         </div>
         <div id={style.text_intro} >
             <TitleIntro val={"La mejor forma de que nos visites..."} ></TitleIntro>
